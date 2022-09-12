ff30c81975eee0701e22d03fa6ad3d454d729a83
Dixit Desai <85556276+dixitdesai@users.noreply.github.com>
2022-09-12 22:04:20 +0530

    Add files via upload

---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,54 @@
+import React from 'react'
+import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+
+import { Search } from '@mui/icons-material'
+import { Paper, IconButton } from '@mui/material'
+
+const SearchBar = () => {
+
+    const [searchTerm, setSearchTerm] = useState('');
+
+    const navigate = useNavigate();
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        if (searchTerm) {
+            navigate(`search/${searchTerm}`)
+            setSearchTerm('');
+        }
+    }
+
+  return (
+    <Paper
+        component="form"
+        onSubmit={handleSubmit}
+        sx={{
+            borderRadius: 20,
+            border: '1px solid #e3e3e3',
+            pl: 2,
+            boxShadow: 'none',
+            mr: { sm: 5 }
+        }}
+    >
+
+        <input 
+            type="text"
+            placeholder='Search...'
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+            className="search-bar"
+        />
+
+        <IconButton 
+            type="submit"
+            sx={{color: 'red', p: '10px'}}
+        >
+            <Search />
+        </IconButton>
+    </Paper>
+  )
+}
+
+export default SearchBar
